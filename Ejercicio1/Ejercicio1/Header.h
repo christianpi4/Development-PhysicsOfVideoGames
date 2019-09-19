de15e36e22e85e30a71b9759ed52555c32463133
polcamacho <47743832+polcamacho@users.noreply.github.com>
2019-09-19 17:37:19 +0200

    functions outside class

--- Ejercicio1/Ejercicio1/Header.h
+++ Ejercicio1/Ejercicio1/Header.h
@@ -12,51 +12,60 @@ class vec3 {
 
 public:
 	
-	vec3(T x1, T y1, T z1) {
-
-		x = x1;
-		y = y1;
-		z = z1;
-
-	}
+	vec3(T, T, T);
 	
-	//~vec3();
-
-	int normalize() {
-
-		float m;
-
-		m = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
-
-		x = x / m;
-		y = y / m;
-		z = z / m;
-
-		return x, y, z;
-
-	}
-
-	int zero() {
-
-		x = 0;
-		y = 0;
-		z = 0;
-
-		return x, y, z;
-
-	}
-
-	bool is_zero() {
-
-
-	}
-
-	float distance_to(vec3 v1, vec3 v2, vec3 v3) {
-
-
-
-	}
+	
+	T normalize();
+	T zero();
+	T is_zero();
+	T distance_to(vec3, vec3, vec3);
 
 };
+template <class T>
+vec3<T>::vec3(T x1, T y1, T z1) {
+
+	x = x1;
+	y = y1;
+	z = z1;
+
+}
+template <class T>
+
+T vec3<T>::normalize() {
+
+	float m;
+
+	m = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
+	x = x / m;
+	y = y / m;
+	z = z / m;
+
+	return x, y, z;
+
+}
+
+template <class T>
+
+T vec3<T>::zero() {
+
+	x = 0;
+	y = 0;
+	z = 0;
+
+	return x, y, z;
+
+}
+template <class T>
+
+T vec3<T>::is_zero() {
 
 
+}
+template <class T>
+
+T vec3<T>::distance_to(vec3 v1, vec3 v2, vec3 v3) {
+
+
+
+}
