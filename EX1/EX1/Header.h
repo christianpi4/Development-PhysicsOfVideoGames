c9d211807277c074828b890baee02143b93f9c0a
christianpi4 <chricraft4100@gmail.com>
2019-09-19 16:48:07 +0200

    Ejercicio1

---
+++ EX1/EX1/Header.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+
+class vec3 {
+	
+	T x, y, z;
+
+public:
+	
+	vec3(T x, T y, T z) {
+
+		x = x1;
+		y = y1;
+		z = z1;
+
+	}
+	
+
+	normalize();
+	zero();
+	is_zero();
+	distance_to(vec3);
+
+};
+
+
---
+++ EX1/EX1/Source.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <locale.h>
+#include <iostream>
+#include "Header.h"
+
+void main() {
+
+	vec3 <int> v1(4, 4, 4);
+	vec3 <float> v2(4.4, 2.4, 5.3);
+	vec3 <double> v3(2.55, 7.44, 8.34); 
+
+
+
+
+
+
+
+
+
+
+
+	system("pause");
+}
