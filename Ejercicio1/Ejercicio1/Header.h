07d3396a1879fc424bd260bb6246712e2c8b1534
christianpi4 <chricraft4100@gmail.com>
2019-09-19 17:15:16 +0200

    Dos primeras funciones

--- Ejercicio1/Ejercicio1/Header.h
+++ Ejercicio1/Ejercicio1/Header.h
@@ -12,7 +12,7 @@ class vec3 {
 
 public:
 	
-	vec3(T x, T y, T z) {
+	vec3(T x1, T y1, T z1) {
 
 		x = x1;
 		y = y1;
@@ -20,11 +20,42 @@ public:
 
 	}
 	
+	//~vec3();
 
-	normalize();
-	zero();
-	is_zero();
-	distance_to(vec3);
+	int normalize() {
+
+		float m;
+
+		m = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+
+		x = x / m;
+		y = y / m;
+		z = z / m;
+
+		return x, y, z;
+
+	}
+
+	int zero() {
+
+		x = 0;
+		y = 0;
+		z = 0;
+
+		return x, y, z;
+
+	}
+
+	bool is_zero() {
+
+
+	}
+
+	float distance_to(vec3 v1, vec3 v2, vec3 v3) {
+
+
+
+	}
 
 };
 
--- Ejercicio1/Ejercicio1/Source.cpp
+++ Ejercicio1/Ejercicio1/Source.cpp
@@ -9,7 +9,7 @@ void main() {
 	vec3 <float> v2(4.4, 2.4, 5.3);
 	vec3 <double> v3(2.55, 7.44, 8.34); 
 
-
+	
 
 
 
