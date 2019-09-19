13701db9845af7ce8f4013a68cfd771741f6a1bb
christianpi4 <chricraft4100@gmail.com>
2019-09-19 22:05:16 +0200

    Menu

--- Ejercicio1/Ejercicio1/Header.h
+++ Ejercicio1/Ejercicio1/Header.h
@@ -12,25 +12,28 @@ class vec3 {
 public:
 	
 	T x, y, z;
-
-
+	
 	vec3(T, T, T);
-		vec3();
+	vec3();
 	
+	void normalize();
+	void writevector();
+	void inicial();
+
+	void zero();
+	bool is_zero();
+	void distance_to(vec3 <int>, vec3 <float>, vec3 <double>, int);
+	/*friend vec3<T> operator +(vec3<T>);
+	friend vec3<T> operator -(vec3<T>);
+	friend vec3<T> operator -=(vec3<T>);
+	friend vec3<T> operator +=(vec3<T>);
+	friend vec3<T> operator ==(vec3<T>);*/
 	
-	T normalize();
-	void writevector(vec3);
-
-	void inicial(vec3);
-
-	T zero();
-	T is_zero();
-	T distance_to(vec3 <int>, vec3 <float>, vec3 <double>, int);
-	friend vec3<T> operator +(vec3<T>&);
 
 };
 
 template <class T>
+
 vec3<T>::vec3() {
 
 	x = 0;
@@ -38,7 +41,9 @@ vec3<T>::vec3() {
 	z = 0;
 
 }
+
 template <class T>
+
 vec3<T>::vec3(T x1, T y1, T z1) {
 
 	x = x1;
@@ -47,10 +52,9 @@ vec3<T>::vec3(T x1, T y1, T z1) {
 
 }
 
-
 template <class T>
 
-T vec3<T>::normalize() {
+void vec3<T>::normalize() {
 
 	float m;
 
@@ -60,90 +64,53 @@ T vec3<T>::normalize() {
 	y = y / m;
 	z = z / m;
 
-	return x, y, z;
+	cout << "(" << x << "," << y << "," << z << ")" << endl;
 
 }
 
-
-
 template <class T>
 
+void vec3<T>::inicial() {
 
-//void vec3<T>::inicial(vec3 v1) {
-//
-//=======
-//void vec3<T>::inicial() {
-//
-//	cout << x << y << z << endl;
-//	cout << "Elige el primer vector de tipo INT: " << endl;
-//
-//	cout << "X: " << endl;
-//	cin >> x;
-//	cout << "Y: " << endl;
-//	cin >> y;
-//	cout << "Z: " << endl;
-//	cin >> z;
-////=======
-////	cout << "X: ";
-////	cin >> x;
-////	cout << endl;
-////	cout << "Y: ";
-////	cin >> y;
-////	cout << endl;
-////	cout << "Z: ";
-////	cin >> z;
-////	cout << endl;
-////
-////	cout << "(" << x << "," << y << "," << z << ")" << endl;
-////
-////>>>>>>> 61816c3e1e6dfd8b5b4171b40e03d9852d027b4b
-//	
-//
-//	/*cout << "Elige el segundo vector de tipo INT: " << endl;
-//	cout << "X: " << endl;
-//	cin >> v2.x;
-//	cout << "Y: " << endl;
-//	cin >> v2.y;
-//	cout << "Z: " << endl;
-//	cin >> v2.z;
-//	system("clear");
-//
-//	cout << "Elige el tercer vector de tipo INT: " << endl;
-//	cout << "X: " << endl;
-//	cin >> v3.x;
-//	cout << "Y: " << endl;
-//	cin >> v3.y;
-//	cout << "Z: " << endl;
-//	cin >> v3.z;
-//	system("clear");*/
-//
-//	
-//	//cout << "(" << v.x << "," << v.y << "," << v.z << ")" << endl;
-//	/*return true;*/
-//}
+	cout << "Elige los vectores el primero INT, segundo FLOAT y el tercero DOUBLE: " << endl;
 
-template <class T>
+	cout << "x: ";
+	cin >> x;
+	cout << endl;
+	cout << "y: ";
+	cin >> y;
+	cout << endl;
+	cout << "z: ";
+	cin >> z;
+	cout << endl;
 
-void vec3<T>::writevector(vec3 v1) {
+	cout << "(" << x << "," << y << "," << z << ")" << endl;
 
-	cout << "(" << v1.x << "," << v1.y << "," << v1.z << ")" << endl;
-	/*return true;*/
 }
 
 template <class T>
 
-T vec3<T>::zero() {
+void vec3<T>::writevector() {
+
+	cout << "(" << x << "," << y << "," << z << ")" << endl;
+	
+}
+
+template <class T>
+
+void vec3<T>::zero() {
 
 	x = 0;
 	y = 0;
 	z = 0;
 
-	return x, y, z;
+	cout << "(" << x << "," << y << "," << z << ")" << endl;
 
 }
 template <class T>
 
-T vec3<T>::is_zero() {
+bool vec3<T>::is_zero() {
+	
 	if (x == 0 && y == 0 && z == 0) {
 		
 		return true;
@@ -153,35 +120,94 @@ T vec3<T>::is_zero() {
 	}
 
 }
+
 template <class T>
 
-T vec3<T>::distance_to(vec3 <int> v1, vec3 <float> v2, vec3 <double> v3, int opcion) {
+void vec3<T>::distance_to(vec3 <int> v1, vec3 <float> v2, vec3 <double> v3, int opcion) {
 	
 	float res1=0, res2=0, res3=0;
+
 	if(opcion==1){
 		
 		res1=pow(pow(v1.x - v2.x,2) + pow(v1.y - v2.y,2) + pow(v1.z - v2.z,2),1/2);
+
 		return res1;
+
 	}
+
 	else if (opcion == 2) {
 
 		res2=pow(pow(v2.x - v3.x, 2) + pow(v2.y - v3.y, 2) + pow(v2.z - v3.z, 2), 1 / 2);
+
 		return res2;
+
 	}
+
 	else if (opcion == 3) {
 
 		res3=pow(pow(v3.x - v1.x, 2) + pow(v3.y - v1.y, 2) + pow(v3.z - v1.z, 2), 1 / 2);
+
 		return res3;
+
 	}
-	
 	
 }
 
-template <class T>
-vec3<T> operator +(vec3<T>& v1) {
-	vec3 <T>res(0, 0, 0);
-	res.x = this->x + v1.x;
-	res.y = this->y + v1.y;
-	res.z = this->z + v1.z;
-	return res;
-}
+//template <class T>
+//
+//vec3<T> operator +(vec3<T>) {
+//	vec3 <T>res(0, 0, 0);
+//	res.x = this->x + v1.x;
+//	res.y = this->y + v1.y;
+//	res.z = this->z + v1.z;
+//	return res;
+//}
+//
+//template <class T>
+//
+//vec3<T> operator -(vec3<T>) {
+//	{
+//		vec3 <T>res(0, 0, 0);
+//		res.x = this->x - v1.x;
+//		res.y = this->y - v1.y;
+//		res.z = this->z - v1.z;
+//		return res;
+//	}
+//}
+//
+//template <class T>
+//
+//vec3<T> operator -=(vec3<T>) {
+//	{
+//		vec3 <T>res(0, 0, 0);
+//
+//		res.x = this->x -= v1.x;
+//		res.y = this->y -= v1.y;
+//		res.z = this->z -= v1.z;
+//		return res;
+//	}
+//}
+//
+//template <class T>
+//
+//vec3<T> operator+=(vec3<T>) {
+//	{
+//		vec3 <T>res(0, 0, 0);
+//		res.x = this->x += v1.x;
+//		res.y = this->y += v1.y;
+//		res.z = this->z += v1.z;
+//		return res;
+//	}
+//}
+//
+//template <class T>
+//
+//vec3<T> operator ==(vec3<T>) {
+//	{
+//		if (this->x == v1->x && this->y == v1->y && this->z == v1->z)
+//		{
+//			return true;
+//		}
+//		return false;
+//	}
+//}
--- Ejercicio1/Ejercicio1/Source.cpp
+++ Ejercicio1/Ejercicio1/Source.cpp
@@ -6,46 +6,96 @@
 void main() {
 
 
-	/*vec3 <int> v1(0, 0, 0);
-	vec3 <float> v2(0, 0, 0);
-	vec3 <double> v3(0, 0, 0);
-	int opcion = 0;*/
+	int opcion = 0;
 
 	vec3 <int> v1(0,0,0);
-	vec3 <float> v2();
-	vec3 <double> v3();
+	vec3 <float> v2(0,0,0);
+	vec3 <double> v3(0,0,0);
 	
-	/*int i;
-
-	cout << " -----------------MENU----------------- " << endl;
-	cout << "1. Normalize" << endl;
-	cout << "2. Zero" << endl;
-	cout << "3. Is Zero?" << endl;
-	cout << "4. Distance To" << endl;
-	cout << "5. Normalizar" << endl;*/
-	/*cout << v1.zero()<<endl;
-	cout << v2.zero()<<endl;*/
-	
-
-	/*v1.inicial(v1);
-	v2.inicial(v2);
-	v3.inicial(v3);
-
-	v1.writevector(v1);
-	v2.writevector(v2);
-	v3.writevector(v3);*/
+	int i;
 
 	v1.inicial();
-	v1.writevector(v1);
+	v2.inicial();
+	v3.inicial();
 
-	//cout << v1.zero() << endl;
-	//cout << v1.is_zero() << endl;
+	do{
 
-	/*cout << "Elige una opcion: ";
-	cin >> opcion;
-	cout << v1.distance_to(v1, v2, v3, opcion) << endl;*/
+		
+		cout << " -----------------MENU----------------- " << endl;
+		cout << "1. Normalize" << endl;
+		cout << "2. Zero" << endl;
+		cout << "3. Is Zero?" << endl;
+		cout << "4. Distance To" << endl;
+		cout << "5. Introducir nuevos vectores" << endl;
+		cout << "6. Mostrar Vectores" << endl;
+		cout << "7. Salir" << endl;
+		
+		cin >> i;
+
+		if (i = 1) {
+			
+			
+			v1.normalize();
+			v2.normalize();
+			v3.normalize();
+
+		}
+
+		else if (i = 2) {
+			
+			
+			v1.zero();
+			v2.zero();
+			v3.zero();
+
+		}
+
+		else if (i = 3) {
+			
+			
+			v1.is_zero();
+			v2.is_zero();
+			v3.is_zero();
+
+		}
+
+		else if (i = 4) {
+
+		
+		/*	v1.distance_to();
+			v2.distance_to();
+			v3.distance_to();*/
+
+		}
+
+		else if (i = 5) {
+
+			v1.inicial();
+			v2.inicial();
+			v3.inicial();
+
+		}
+
+		else if (i = 6) {
+
+			
+			v1.writevector();
+			v2.writevector();
+			v3.writevector();
+
+		}
+		
+		else if (i = 7) {
+
+			system("pause");
+
+		}
+
+		
+		//cout << v1.distance_to(v1, v2, v3, opcion) << endl;*/
 
 
+	} while (i != 7);
 
 	system("pause");
 }
